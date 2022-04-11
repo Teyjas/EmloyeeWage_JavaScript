2be57207f5f49e421dda451afce6e215aa2bb3cf
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-04-11 19:59:01 +0530

    [Teyjas]Add:EmployeeWage_UseCases

---
+++ EmpWage_UC1.js
@@ -0,0 +1,9 @@
+// check employee is present or absent
+const ABSENT=0;
+let employeecheck = Math.floor(Math.random()*10)%2;
+  if(employeecheck== ABSENT){
+      console.log("Employee Is Absent")
+  }
+  else{
+      console.log("Employee Is Present")
+  };
